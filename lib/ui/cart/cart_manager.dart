e77c4a52e8af8694744d6640ae8ba704a0206fd3
kietb1910089 <kietb1910089@student.ctu.edu.vn>
2022-09-21 01:30:27 +0700

    Xay dung trang gio hang

---
+++ lib/ui/cart/cart_manager.dart
@@ -0,0 +1,34 @@
+import 'package:myshop/models/product.dart';
+
+import '../../models/cart_item.dart';
+
+class CartManager {
+  final Map<String, CartItem> _items = {
+    'p1': CartItem(
+      id: 'p1',
+      title: 'Red Shirt',
+      quantity: 2,
+      price: 29.99,
+    ),
+  };
+
+  int get productCount {
+    return _items.length;
+  }
+
+  List<CartItem> get products {
+    return _items.values.toList();
+  }
+
+  Iterable<MapEntry<String, CartItem>> get productEntries {
+    return {..._items}.entries;
+  }
+
+  double get totalAmount {
+    var total = 0.0;
+    _items.forEach((key, cartItem) {
+      total += cartItem.price * cartItem.quantity;
+    });
+    return total;
+  }
+}
