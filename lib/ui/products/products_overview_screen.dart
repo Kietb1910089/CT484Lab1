27f6ed990e6878cb73d780d8475f68317175cd2b
kietb1910089 <kietb1910089@student.ctu.edu.vn>
2022-10-29 23:18:35 +0700

    Luu danh muc san pham vao Realtime Database cua Firebase

--- lib/ui/products/products_overview_screen.dart
+++ lib/ui/products/products_overview_screen.dart
@@ -4,6 +4,7 @@ import '../cart/cart_screen.dart';
 import 'products_grid.dart';
 import '../shared/app_drawer.dart';
 import '../cart/cart_manager.dart';
+import 'products_manager.dart';
 import 'top_right_badge.dart';
 
 enum FilterOptions { favorites, all }
@@ -16,7 +17,14 @@ class ProductsOverviewScreen extends StatefulWidget {
 }
 
 class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
-  var _showOnlyFavorites = false;
+  final _showOnlyFavorites = ValueNotifier<bool>(false);
+  late Future<void> _fetchProducts;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchProducts = context.read<ProductsManager>().fetchProducts();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,7 +37,21 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
         ],
       ),
       drawer: const AppDrawer(),
-      body: ProductsGrid(_showOnlyFavorites),
+      body: FutureBuilder(
+        future: _fetchProducts,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return ValueListenableBuilder<bool>(
+                valueListenable: _showOnlyFavorites,
+                builder: (context, onlyFavorites, child) {
+                  return ProductsGrid(onlyFavorites);
+                });
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
     );
   }
 
@@ -54,13 +76,11 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
   Widget buildProductFilterMenu() {
     return PopupMenuButton(
       onSelected: (FilterOptions selectedValue) {
-        setState(() {
-          if (selectedValue == FilterOptions.favorites) {
-            _showOnlyFavorites = true;
-          } else {
-            _showOnlyFavorites = false;
-          }
-        });
+        if (selectedValue == FilterOptions.favorites) {
+          _showOnlyFavorites.value = true;
+        } else {
+          _showOnlyFavorites.value = false;
+        }
       },
       icon: const Icon(
         Icons.more_vert,
